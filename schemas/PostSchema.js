1bed2a85a57d0afdf0d623875d6c2bbcfbd71219
Adetayo <twinebravo@gmail.com>
2025-04-19 10:01:21 +0100

    Added Oupting post to news feed

---
+++ schemas/PostSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    content: { type: String, trim: true },
+    postedBy: { type: Schema.Types.ObjectId, ref: 'User' },
+    pinned: Boolean,
+    monetise: { type: Boolean, default: false }
+}, { timestamps: true });
+
+
+var Post = mongoose.model('Post', PostSchema);
+module.exports = Post;
