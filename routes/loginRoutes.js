e084660d1cc0006768cecddaa3a68b09aa505deb
Adetayo <twinebravo@gmail.com>
2025-04-28 13:53:54 +0100

    Updated loginRoutes.js file

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,28 +1,19 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
-
 const User = require('../schemas/UserSchema');
 
-
-app.set("view engine", "pug");
-app.set("views", "views");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
+router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", (req, res, next) => {
     res.status(200).render("login");
-})
-
+});
 
 router.post("/", async (req, res, next) => {
-
     var payload = req.body;
 
-    if(req.body.logUsername && req.body.logPassword) {
+    if (req.body.logUsername && req.body.logPassword) {
         var user = await User.findOne({
             $or: [
                 { username: req.body.logUsername },
@@ -34,12 +25,10 @@ router.post("/", async (req, res, next) => {
             payload.errorMessage = "Something went wrong.";
             res.status(200).render("login", payload);
         });
-        
 
-        if(user != null) {
+        if (user != null) {
             var result = await bcrypt.compare(req.body.logPassword, user.password);
-
-            if(result === true) {
+            if (result === true) {
                 req.session.user = user;
                 return res.redirect("/");
             }
@@ -50,9 +39,7 @@ router.post("/", async (req, res, next) => {
     }
 
     payload.errorMessage = "Make sure each field has a valid value.";
-    res.status(200).render("login");
-})
-
-
+    res.status(200).render("login", payload);
+});
 
 module.exports = router;
